ffa052b3aeb8ae842184d07d1989c6af7ea0e1cd
carlos <carlosantoniodelfino@hotmail.com>
2017-04-03 22:30:25 -0300

    alterações classes servico

--- plataformaservicosweb/src/main/java/edu/cesusc/servico/ServicoDAO.java
+++ plataformaservicosweb/src/main/java/edu/cesusc/servico/ServicoDAO.java
@@ -1,11 +1,5 @@
 package edu.cesusc.servico;
 
-public interface ServicoDAO {
-	public void carregar (Integer id_Servico);
-	public void salvar(Servico servico);
-	public void atualizar(Servico servico);
-	public void excluir(Servico servico);
-	public void listar();
+public class ServicoDAO {
+
 }
-
-
--- plataformaservicosweb/src/main/java/edu/cesusc/servico/ServicoDAOHibernate.java
+++ plataformaservicosweb/src/main/java/edu/cesusc/servico/ServicoDAOHibernate.java
@@ -1,35 +1,5 @@
 package edu.cesusc.servico;
-import java.util.List;
-import org.hibernate.Query;
-import org.hibernate.Session;
 
-import edu.cesusc.seguranca.usuario.Usuario;
-
-public class ServicoDAOHibernate implements ServicoDAO {
-	private Session session;
-
-	public void setSession(Session session) {
-		this.session = session;
-	}
-
-	public void salvar(Servico servico) {
-		this.session.save(servico);
-	}
-
-	public void atualizar(Servico servico) {
-		this.session.update(servico);
-	}
-
-	public void excluir(Servico servico) {
-		this.session.delete(servico);
-	}
-
-	public Servico carregar(Integer id_Servico) {
-		return (Servico) this.session.get(Servico.class, id_Servico);
-	}
-
-	public List<Servico> listar() {
-		return this.session.createCriteria(Servico.class).list();
-	}
+public class ServicoDAOHibernate {
 
 }
