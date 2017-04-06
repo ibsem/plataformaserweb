2ccf2685a21d8883d34617822b95b69a04a32396
carlos <carlosantoniodelfino@hotmail.com>
2017-04-05 21:55:39 -0300

    Atualizaçao das classes servicos

--- plataformaservicosweb/src/main/java/edu/cesusc/servico/ServicoDAO.java
+++ plataformaservicosweb/src/main/java/edu/cesusc/servico/ServicoDAO.java
@@ -1,5 +1,19 @@
 package edu.cesusc.servico;
 
-public class ServicoDAO {
+import java.util.List;
+
+import edu.cesusc.seguranca.usuario.Usuario;
+
+
+	public interface ServicoDAO {
+		public void mostrar(Servico servico);
+		public void Buscar(Usuario usuario);
+		public void atualizar(Servico servico);
+		public void inserir(Servico servico);
+		public void alterar(Servico servico);;
+		public void salvar(Servico servico);
+		public void excluir(Servico servico);
+		public Servico carregar(Integer id_servico);
+		public List<Servico> listar();
 
 }
